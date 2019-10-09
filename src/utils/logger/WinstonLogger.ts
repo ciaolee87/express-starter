bcf74f640c179a572be278ddcd122397f16ec9f4
Ciao Lee <ciaolee87@gmail.com>
2019-10-09 18:18:29 +0900

    로그 저장 폴더 절대 경로로 수정

--- src/utils/logger/WinstonLogger.ts
+++ src/utils/logger/WinstonLogger.ts
@@ -3,7 +3,6 @@ import winston from 'winston';
 import fs from 'fs';
 import {ConsoleTransport} from "./ConsoleTransport";
 import {DailyTransport} from "./DailyTransport";
-import * as path from "path";
 
 // 로거 단계
 // 0: error
@@ -14,25 +13,20 @@ import * as path from "path";
 // 5: silly
 
 // 로그 파일 저장할 폴더 만들기
-const logPath = path.join(__dirname, process.env.LOG_PATH);
+const logPath = process.env.LOG_PATH;
 if (!fs.existsSync(logPath)) {
-    fs.mkdirSync(logPath);
+	fs.mkdirSync(logPath);
 }
 
 // 로거 생성
-const env = process.env.NODE_ENV;
 let outs = [];
 outs.push(DailyTransport);
-
-// 테스트나 개발시 콘솔에 메시지를 표시한다
-if (env == 'development' || env == "test") {
-    outs.push(ConsoleTransport);
-}
+outs.push(ConsoleTransport);
 
 // 로거 객체 생성
 const Logger = winston.createLogger({
-    level: process.env.LOG_LEVEL || 'debug',
-    transports: outs
+	level: process.env.LOG_LEVEL || 'debug',
+	transports: outs
 });
 
 export default Logger;
