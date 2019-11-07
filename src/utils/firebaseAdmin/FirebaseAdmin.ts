17e76ff4e86a485c918a4534eb1e430c9b8355c0
Ciao Lee <ciaolee87@gmail.com>
2019-11-07 21:58:04 +0900

    Firebase 모듈 수정

--- src/utils/firebaseAdmin/FirebaseAdmin.ts
+++ src/utils/firebaseAdmin/FirebaseAdmin.ts
@@ -2,4 +2,8 @@ import * as firebase from "firebase-admin";
 
 // 파이어 베이스 초기화
 const config = require(process.env.FIREBASE_KEY);
-export const FirebaseAdmin = firebase.initializeApp(config);
+const FirebaseAdmin = firebase.initializeApp({
+	credential: firebase.credential.cert(config)
+});
+
+export default FirebaseAdmin;
